aa780a769586284399b0d1a9ffb85ee9fa38c1f2
Darsa Dev <devdarsa@darsa.id>
2026-08-06 06:46:08 +0700

    fix(prisma): generate client to root node_modules for Vercel Monorepo engine resolution

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../node_modules/.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -12,4 +12,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export * from '../node_modules/.prisma/client';
+export * from '@prisma/client';
